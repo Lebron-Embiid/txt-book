a798f17d57b4c6f5618a26d645eb7e8221f2d9b4
lebron-embiid <973265025@qq.com>
2019-03-14 16:38:30 +0800

    add flexible rem.

--- 响应式默认文件/index.php
+++ 响应式默认文件/index.php
@@ -8,8 +8,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
 	<meta name="renderer" content="webkit">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" href="css/common.css">
 	<link rel="stylesheet" href="css/index.css">
+	<script src="js/rem.js"></script>
 	<script src="js/jquery-2.1.4.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 </head>
